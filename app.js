da85ebe1f80a2ff093275aca2902c70945178aac
Arthur <aquarius0256@mail.ru>
2021-09-06 22:31:18 +0300

    Initial commit, create app.js and package.json

---
+++ app.js
@@ -0,0 +1,7 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+
+const app = express();
